2aaef4b2721a89e20425b369b3ec3fd156a434a0
vermosen <vermosen.jm@free.fr>
2014-04-06 12:16:38 -0400

    created termstructure management

--- LiborModel/main.cpp
+++ LiborModel/main.cpp
@@ -1,11 +1,78 @@
+/*
+ * 
+ * The goal of this project is to calibrate blabla...
+ *
+ *
+ *
+ *
+ *
+ */
+
+
 #include <ql/quantlib.hpp>
+
+using namespace QuantLib;
+
+static struct dataSet {
+
+	Date reference_ = Date(7, April, 2014);
+	Calendar calendar_ = QuantLib::UnitedStates(QuantLib::UnitedStates::Settlement);
+
+
+};
+
+// build the termstructure
+boost::shared_ptr<YieldTermStructure> termStructure(const dataSet & ds) {		
+
+	Date reference(7, April, 2014);							// reference date
+	boost::shared_ptr<YieldTermStructure> ts(
+		new FlatForward(ds.reference_,
+						0.05, Actual365Fixed(), 
+						Continuous, Annual));
+
+	return ts;
+
+}
 
 int main() {
 
-	QuantLib::UnitedStates calendar(QuantLib::UnitedStates::Settlement);
+	try {
 
-	std::cout << calendar.name() << std::endl;
+		dataSet ds;
 
-	return 1;
+		// creating the yield curve
+		boost::shared_ptr<YieldTermStructure> ts = termStructure(ds);
+
+		// creating the correlation matrix
+
+		// creating .csv file
+		QuantLib::utilities::csvBuilder test("C://Temp/test.csv");
+
+		test.add(10.0, 1, 1);
+
+		system("pause");										// console makes a pause
+
+		return 0;
+
+	}
+	catch (std::exception & e) {
+	
+		std::cout << "an error occurred: "
+			<< std::endl
+			<< e.what()
+			<< std::endl;
+
+		system("pause");
+		return 1;
+	
+	}
+	catch (...) {
+
+		std::cout << "an unknown error occurred" << std::endl;
+		system("pause");
+		return 1;
+
+	}
+
 
 }
