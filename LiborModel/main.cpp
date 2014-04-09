c0fadda71c029199e5b7e0ddab1db22d99fdebc2
vermosen <vermosen.jm@free.fr>
2014-04-09 17:17:30 -0400

    added evolution description generation

--- LiborModel/main.cpp
+++ LiborModel/main.cpp
@@ -16,44 +16,88 @@ struct dataSet {										// static struct
 
 	/* general data */
 	Date reference_ = Date(7, April, 2014);
+
 	Calendar calendar_ = QuantLib::UnitedStates(QuantLib::UnitedStates::Settlement);
 
-	std::vector<Time> maturities_ = std::vector<Time>{0.10, 0.25, 0.50, 0.75, 1.0, 1.5, 2.0};
+	/* initial rates */
+	std::vector<Time> initialRates_ = std::vector<Time>{
+		.15, .18, .20, .23, .04, 1.5, 2.0};
+
+	/* rate maturities (temp) */
+	std::vector<Time> maturities_ = std::vector<Time>{
+		.10, .25, .50, .75, 1.0, 1.5, 2.0};
+
+	std::vector<Volatility> volatilities_ = std::vector<Volatility>{
+		.05, .05, .05 , .045, .045, .04, .04};
+
+	/* spreads.. Check what is the purpose here... */
+	std::vector<Spread> displacements_ = std::vector<Spread>{
+		.0, .0, .0, .0, .0, .0, .0};
+
+	Size numberOfFactors_ = std::min<Size>((Size)5, maturities_.size());
+
+	Real volLevel_ = 0.11;
+	
+	Real beta_ = 0.2;
+	
+	Real gamma_ = 1.0;
+	
+	Size numberOfFactors_ = std::min<Size>(5, maturities_.size());
 
 	/* termstructure building */
 	boost::shared_ptr<YieldTermStructure> termStructure() {
 
-		boost::shared_ptr<YieldTermStructure> ts(
+		return boost::shared_ptr<YieldTermStructure>(
 			new FlatForward(
 				this->reference_,
 				0.05, Actual365Fixed(),
 				Continuous, Annual));
 
-		return ts;
-
 	}
 
-	/* termstructure building */
+	/* correlation structure */
 	boost::shared_ptr<PiecewiseConstantCorrelation> correlations() {
 
 		Real volLevel = 0.11;
 		Real beta = 0.2;
 		Real gamma = 1.0;
-		Size numberOfFactors = std::min<Size>(5, maturities_.size());
 
-		boost::shared_ptr<PiecewiseConstantCorrelation> corr(
+		return boost::shared_ptr<PiecewiseConstantCorrelation>(
 			new ExponentialForwardCorrelation(
 				maturities_,
 				volLevel, 
 				beta, gamma));
 
-		return corr;
+	}
 
+	/* volatility structure initializer */
+	boost::shared_ptr<MarketModel> model() {
+
+		return boost::shared_ptr<MarketModel>(
+			new FlatVol(
+				volatilities_,
+				correlations(),
+				*evolution(),
+				numberOfFactors_,
+				initialRates_,
+				displacements_));
+
+	}
+
+	// create the evolution description
+	boost::shared_ptr<EvolutionDescription> evolution() {
+	
+		return boost::shared_ptr<EvolutionDescription>(
+			new EvolutionDescription(
+				maturities_));
+	
 	}
 
 };
 
 static dataSet tt;												// global variable declaration
+
+const Size seed = 12332;										// define a seed for simulation
 
 int main() {
 
@@ -65,7 +109,12 @@ int main() {
 		// load the correlations
 		Matrix corr = tt.correlations()->correlation(0);
 
-		// creating the correlation matrix
+		// creating the model
+		boost::shared_ptr<MarketModel> model = tt.model();
+
+		// create a sobol BM generator factory
+		SobolBrownianGeneratorFactory generatorFactory(
+			SobolBrownianGenerator::Diagonal, seed);
 
 		// creating .csv file
 		QuantLib::utilities::csvBuilder test("C://Temp/test.csv");
